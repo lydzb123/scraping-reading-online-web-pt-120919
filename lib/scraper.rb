3022d1319839537da68cf01d70924b57da99923f
Lydia B <lydiabaik@gmail.com>
2020-02-01 21:08:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-courses = doc.css(".title-oE5vT4").attribute
+courses = doc.css(".title-oE5vT4")
 
 courses.each do |course|
   puts course.text.strip
