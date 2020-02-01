e488a10d298ac66074e46aa1ab3c16e9f81f2a4a
Lydia B <lydiabaik@gmail.com>
2020-02-01 21:01:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".title-oE5vT4").text
+binding.pry
